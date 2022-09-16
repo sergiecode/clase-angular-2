4e86cb6e0649267cb9cde5cb38e0e973ecaab94c
SnSDk <sergiosnsdk@gmail.com>
2022-09-16 14:52:35 -0300

    Se agrega un objeto para consumir

---
+++ src/app/components/mocks/objeto.mock.ts
@@ -0,0 +1,18 @@
+export const arrayDeObjetos = [
+    {
+        nombre: "Rhaenyra Targaryen",
+        mail: "rhaenyra@gmail.com"
+    },
+    {
+        nombre: "Alicent Hightower",
+        mail: "alicent@gmail.com"
+    },
+    {
+        nombre: "Corlys Velaryon",
+        mail: "corlys@gmail.com"
+    },
+    {
+        nombre: "Tyrion Lannister",
+        mail: "tyrion@gmail.com"
+    }
+]
